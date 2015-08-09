355ffac2c8b5bcb9420a35ba84c8c89fd68d7aa2
Migz <migzwebhost@gmail.com>
2015-08-09 14:04:10 +0800

    Connected web to cookstogo API basics(recipes)

--- CooksToGo/settings.py
+++ CooksToGo/settings.py
@@ -84,11 +84,10 @@ DATABASES = {
     }
 }
 '''
-
 import dj_database_url
 
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
